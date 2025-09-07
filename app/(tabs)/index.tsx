41b060e08e1bf6df6f84dd7591ec1cde5be58952
RithmaRandima <your-correct-email@example.com>
2025-09-07 13:13:31 +0530

    adding style files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,40 @@
-import useTheme from "@/hooks/useTheme";
-import { Text, TouchableOpacity, View } from "react-native";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+
+  const styles = createStyles(colors);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>editsd tiscreen.</Text>
+
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Mode</Text>
       </TouchableOpacity>
+
+      {/* <TouchableOpacity onPress={() => addTodo({ text: "Walk the dog" })}>
+        <Text>Add a Todo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>Clear All Todos</Text>
+      </TouchableOpacity> */}
     </View>
   );
 }
+
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 10,
+      backgroundColor: colors.bg,
+    },
+  });
+
+  return styles;
+};
